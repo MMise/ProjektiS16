de2e0fe2891c11e0cd9dd7c54f6b93693efb2c26
komuvill <V-V.Komulainen@hotmail.com>
2016-11-03 10:05:13 +0200

    Add files via upload

---
+++ dht11-sensoritesti.ino
@@ -0,0 +1,19 @@
+#include <dht.h>
+//määritellään sensorin pinni
+dht DHT;
+
+#define DHT11_PIN 2
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  Serial.print("Lampotila = ");
+  Serial.println(DHT.temperature); //printataan sarjamonitorille lämpötila
+  Serial.print("Ilmankosteus = %");
+  Serial.println(DHT.humidity); //printataan sarjamonitorille ilmankosteus
+  delay(1000);
+}
