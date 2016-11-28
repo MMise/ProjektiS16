b5379ff2201b5ce8d940c5b3ac0049ca8eefaf14
Mikko Jokinen <mikko.jokinen96@gmail.com>
2016-11-28 11:09:33 +0200

    Add files via upload

---
+++ dht22final.ino
@@ -0,0 +1,21 @@
+#include <DHT.h>
+#include <DHT_U.h>
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup(){
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop()
+{
+  float kosteus = dht.readHumidity();
+  float temp = dht.readTemperature();
+  Serial.print("Lampotila = ");
+  Serial.println(temp);
+  Serial.print("Ilmankosteus = ");
+  Serial.println(kosteus);
+  delay(2000);
+}
