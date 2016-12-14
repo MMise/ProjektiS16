d775137d7e77903c0e9b125fa55ed68834b0214c
komuvill <V-V.Komulainen@hotmail.com>
2016-12-14 17:04:21 +0200

    Update weatherstation_advanced.ino

--- weatherstation_advanced.ino
+++ weatherstation_advanced.ino
@@ -9,6 +9,7 @@ BMP280 bmp280;
 
 //DS1307-KELLOMODUULIN ALUSTUS
 #define DS1307_I2C_ADDRESS 0x68
+#include "Wire.h"
  
 //DHT22-SENSORIN ALUSTUS
 #include <DHT.h>
@@ -20,6 +21,25 @@ DHT dht(DHTPIN, DHTTYPE);
 //BLUETOOTHILLA LÄHETETTÄVIEN TEKSTIJONOJEN MUUTTUJAT
 char junk;
 String inputString = "";
+
+//MUUTTUJAT KESKIARVOJEN LASKENTAAN LASKENTAAN
+float tunninpaine;
+float tunninlampo;
+float tunninkosteus;
+float paivanpaine;
+float paivanlampo;
+float paivankosteus;
+float keskipaine;
+float keskilampo; 
+float keskikosteus;
+float keskipaineH;
+float keskilampoH; 
+float keskikosteusH;
+
+
+//LASKURI
+int i; //tunnin keskiarvoille
+int j; //päivän keskiarvoille
 
 //MUUTTUJAT, JOIHIN TALLENNETAAN MAKSIMI- JA MINIMIARVOT
 
@@ -143,34 +163,6 @@ void displayTime()
   }
 }
 
-float haeAika(byte second, byte minute, byte hour, byte dayOfMonth, byte month, byte year)
-{
-  readDS1307time(&second, &minute, &hour, &dayOfMonth, &month, &year);
-  if (second == 1)
-  {
-    return second, DEC;
-  }
-  else if (minute == 1)
-  {
-    return minute, DEC;
-  }
-  else if (hour == 1)
-  {
-    return hour, DEC;
-  }
-  else if (dayOfMonth == 1)
-  {
-    return dayOfMonth, DEC;
-  }
-  else if (month == 1)
-  {
-    return month, DEC;
-  }
-  else if (year == 1)
-  {
-    return year, DEC;
-  }
-}
 
 //ALUSTETAAN SENSORIT JA LCD-NÄYTTÖ
 void setup()
@@ -186,7 +178,7 @@ void setup()
   // seuraavassa osassa asetetaan aika sulkujen sisään seuraavassa järjestyksessä, pilkulla erottaen:
   // sekunnit, minuutit, tunnit, päivämäärä, viikonpäivä (1 = SU, 2 = MA jne), kuukausi, vuosi (00-99)
   
-/*  setDS1307time(10,58,10,3,29,11,16);  */ 
+  /*setDS1307time(55,02,11,5,1,12,16);*/ 
 /*
 Tämä pitää kommentoida pois onnistuneen ajan asetuksen jälkeen,
 ja tämän jälkeen sketsi ladataan uudestaan Arduinon muistiin.
@@ -196,6 +188,22 @@ Muuten Arduino asettaa ajaksi aina käynnistyessään funktioon asetetun ajan.
 
 void loop()
 {
+  /*määritellään aikamuuttujat uudestaan erillään funktioista, jotta saadaan ohjelma
+  tekemään juttuja tietyllä kellonajalla*/
+  byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+  Wire.beginTransmission(DS1307_I2C_ADDRESS);
+  Wire.write(0); // Asettaa DS1307:n rekisteriosoittimen osoitteeseen 00h
+  Wire.endTransmission();
+  Wire.requestFrom(DS1307_I2C_ADDRESS, 7);
+  // Pyytää DS1307:lta 7 bittiä dataa alkaen rekisteristä 00h
+ 
+  second = bcdToDec(Wire.read());
+  minute = bcdToDec(Wire.read());
+  hour = bcdToDec(Wire.read());
+  dayOfWeek = bcdToDec(Wire.read());
+  dayOfMonth = bcdToDec(Wire.read());
+  month = bcdToDec(Wire.read());
+  year = bcdToDec(Wire.read());
   
   displayTime(); // Aika LCD-näytölle
    
@@ -217,44 +225,6 @@ void loop()
 
   float temperature = (temperature280 + temperature22) / 2;
   
-/*
-
-Maksimien ja minimien laskemiset tietyllä aikavälillä, vaatii vielä työtä.
-
-  {
-    float maxTemp = temperature;
-    float minTemp = temperature;
-    float maxPress = hPa;
-    float minPress = hPa;
-    float maxHum = humidity;
-    float minHum = humidity;
-  }
-  if (temperature > maxTemp)
-  {
-    maxTemp = temperature;
-  }
-  if (temperature < minTemp)
-  {
-    minTemp = temperature;
-  }
-  if (hPa > maxPress)
-  {
-    maxPress = hPa;
-  }
-  if (hPa < minPress)
-  {
-    minPress = hPa;
-  }
-  if (humidity > maxHum)
-  {
-    maxHum = humidity;
-  }
-  if (humidity < minHum)
-  {
-    minHum = humidity;
-  }
-*/
-
   //Tulostetaan loputkin tiedot näytölle:
   lcd.setCursor(0, 1);
   lcd.print(temperature);
@@ -265,7 +235,86 @@ Maksimien ja minimien laskemiset tietyllä aikavälillä, vaatii vielä työtä.
   lcd.setCursor(0, 3);
   lcd.print(humidity);
   lcd.print(" %");
-  delay(250);
+  delay(500);
+
+  if (minute % 10 == 0 && second == 0)
+  {
+    tunninpaine = tunninpaine + hPa;
+    tunninlampo = tunninlampo + temperature;
+    tunninkosteus = tunninkosteus + humidity;
+    i++;
+    delay(1000); //odotellaan hieman, jotta lukemia ei tallennu yhdelle mittauskerralle useita
+  
+  
+    if (i == 6)
+    {
+      keskipaineH = tunninpaine / 6;
+      keskilampoH = tunninlampo / 6; 
+      keskikosteusH = tunninkosteus / 6;
+      tunninpaine = 0;
+      tunninlampo = 0;
+      tunninkosteus = 0;
+      i = 0;
+    }
+  }
+
+  if (minute % 59 == 0 && second == 0)
+  {
+    paivanpaine = paivanpaine + hPa;
+    paivanlampo = paivanlampo + temperature;
+    paivankosteus = paivankosteus + humidity;
+    j++;
+    delay(1000);
+    
+    //Maksimien ja minimien tallentamiset
+    
+    if (j == 0) //Asetetaan sensorilukemat maksimi- ja minimiarvoiksi ensimmäisellä mittauskerralla
+      {
+        maxTemp = temperature;
+        minTemp = temperature;
+        maxPress = hPa;
+        minPress = hPa;
+        maxHum = humidity;
+        minHum = humidity;
+      }
+      
+      if (temperature > maxTemp)
+      {
+        maxTemp = temperature;
+      }
+      if (temperature < minTemp)
+      {
+        minTemp = temperature;
+      }
+      if (hPa > maxPress)
+      {
+        maxPress = hPa;
+      }
+      if (hPa < minPress)
+      {
+        minPress = hPa;
+      }
+      if (humidity > maxHum)
+      {
+        maxHum = humidity;
+      }
+      if (humidity < minHum)
+      {
+        minHum = humidity;
+      }
+  
+    if (j == 24)
+    {
+        keskipaine = paivanpaine / 24;
+        keskilampo = paivanlampo / 24; 
+        keskikosteus = paivankosteus / 24;
+        paivanpaine = 0;
+        paivanlampo = 0;
+        paivankosteus = 0;
+        j = 0;
+    }
+  }
+  
 
   //Tarkistetaan onko Bluetooth-yhteyden yli tulossa tekstijonoja
   
@@ -283,21 +332,29 @@ Maksimien ja minimien laskemiset tietyllä aikavälillä, vaatii vielä työtä.
    
    //Arvioitu korkeus merenpinnasta:
     
-   if(inputString == "korkeus")
-   {
-     float korkeus;
-     bmp280.getAltitude(korkeus);
-     lcd.clear();
-     lcd.setCursor(0,0);
-     lcd.print("Arvioitu korkeus");
-     lcd.setCursor(0,1);
-     lcd.print("merenpinnasta:");
-     lcd.setCursor(0,2);
-     lcd.print(korkeus);
-     lcd.print(" metria.");
-     delay(5000);
-     lcd.clear();  
-   }
+    if (inputString == "korkeus")
+    {
+      float korkeus;
+      float syotettyPaine;
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Odotetaan tietoja.");
+      Serial.println("Anna paine merenpinnan tasolla:");
+      Serial.flush(); //Tyhjennetään sarjamonitorin puskuri
+      while(!Serial.available()); //Odotellaan syötettä
+      syotettyPaine = Serial.parseFloat();
+      Serial.println(syotettyPaine);
+      korkeus = 44330 * (1.0 - pow(hPa/syotettyPaine, 0.1903));
+      lcd.setCursor(0,0);
+      lcd.print("Arvioitu korkeus");
+      lcd.setCursor(0,1);
+      lcd.print("merenpinnasta:");
+      lcd.setCursor(0,2);
+      lcd.print(korkeus);
+      lcd.print(" metria.");
+      delay(5000);
+      lcd.clear();
+    }
    
    //Maksimit ja minimit:
 
@@ -337,26 +394,106 @@ Maksimien ja minimien laskemiset tietyllä aikavälillä, vaatii vielä työtä.
       Serial.println(" hPa");
       Serial.print(humidity);
       Serial.println(" %");
-      delay(5000);
+      delay(10000);
     }
    
-   //Kellonjana muuttaminen, vaatii vielä työtä.
+   //Kellonajan muuttaminen
    
-     /*
+     
     if(inputString == "aseta")
     {
+      lcd.clear();
+      lcd.setCursor(0,1);
+      lcd.print("Aseta aika");
+      lcd.setCursor(0,2);
+      lcd.print("sarjamonitorissa!");
       Wire.beginTransmission(DS1307_I2C_ADDRESS);
       Wire.write(0);
       Serial.println("Anna sekunnit:");
-      Wire.write(decToBcd(second = Serial.read())); // sekunnit
+      Serial.flush();
+      while(!Serial.available());
+      Wire.write(decToBcd(second = Serial.parseInt())); // sekunnit
+      Serial.println(second);
       Serial.println("Anna minuutit:");
-      Wire.write(decToBcd(minute = Serial.read())); // minuutit
+      Serial.flush();
+      while(!Serial.available());
+      Wire.write(decToBcd(minute = Serial.parseInt())); // minuutit
+      Serial.println(minute);
       Serial.println("Anna tunnit:");
-      Wire.write(decToBcd(hour = Serial.read())); // tunnit
+      Serial.flush();
+      while(!Serial.available());
+      Wire.write(decToBcd(hour = Serial.parseInt())); // tunnit
+      Serial.println(hour);
       Wire.endTransmission();
     }
-    */
-   
+
+    if(inputString = "tunti") 
+    {
+      lcd.clear();
+      Serial.print("Arvoja laskettu summaan:");
+      Serial.println(i);
+      Serial.println("ARVOJEN SUMMA:");
+      Serial.print("C.sum: ");
+      Serial.println(tunninlampo);
+      Serial.print("hPa.sum: ");
+      Serial.println(tunninpaine);
+      Serial.print("Hum.sum: ");
+      Serial.println(tunninkosteus);
+      Serial.println("KESKIARVOT:");
+      Serial.print("C: ");
+      Serial.println(keskilampoH);
+      Serial.print("hPa: ");
+      Serial.println(keskipaineH);
+      Serial.print("Hum:");
+      Serial.println(keskikosteusH);
+      lcd.setCursor(0,1);
+      lcd.print("KESKIARVOT:");
+      lcd.setCursor(0,2);
+      lcd.print(keskilampoH);
+      lcd.print(" C");
+      lcd.setCursor(0,3);
+      lcd.print(keskipaineH);
+      lcd.print(" hPa");
+      lcd.setCursor(0,4);
+      lcd.print(keskikosteusH);
+      lcd.print(" %");
+      delay(10000);
+      lcd.clear();
+    }
+
+    if(inputString = "paiva") 
+    {
+      lcd.clear();
+      Serial.print("Arvoja laskettu summaan:");
+      Serial.println(j);
+      Serial.println("ARVOJEN SUMMA:");
+      Serial.print("C.sum: ");
+      Serial.println(paivanlampo);
+      Serial.print("hPa.sum: ");
+      Serial.println(paivanpaine);
+      Serial.print("Hum.sum: ");
+      Serial.println(paivankosteus);
+      Serial.println("KESKIARVOT:");
+      Serial.print("C: ");
+      Serial.println(keskilampo);
+      Serial.print("hPa: ");
+      Serial.println(keskipaine);
+      Serial.print("Hum:");
+      Serial.println(keskikosteus);
+      lcd.setCursor(0,1);
+      lcd.print("KESKIARVOT:");
+      lcd.setCursor(0,2);
+      lcd.print(keskilampo);
+      lcd.print(" C");
+      lcd.setCursor(0,3);
+      lcd.print(keskipaine);
+      lcd.print(" hPa");
+      lcd.setCursor(0,4);
+      lcd.print(keskikosteus);
+      lcd.print(" %");
+      delay(10000);
+      lcd.clear();
+    }  
    //top kek -tier komennot
    
    if(inputString == "fitta")
@@ -384,13 +521,13 @@ Maksimien ja minimien laskemiset tietyllä aikavälillä, vaatii vielä työtä.
     lcd.setCursor(0,1);
     lcd.print("sequence initiated");
     lcd.setCursor(9,3);
-    for(int i = 5; i < 1; i--)
+    delay(1000); 
+    for(int k = 5; i < 1; i--)
     {
        lcd.print("T-");
-       lcd.print(i);
+       lcd.print(k);
        delay(1000);
     }
-    lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Sequence failed.");
     lcd.setCursor(0,1);
